f8cf4a18b9965621c609701f4b675fb1ca268519
taraswithjazz <43855967+taraswithjazz@users.noreply.github.com>
2023-03-31 23:34:06 +0300

    Added seed data and brand + types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -9,25 +10,36 @@ namespace API.Controllers
 	[Route("api/[controller]")]
 	public class ProductsController : ControllerBase
 	{
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         [HttpGet]
 		public async Task<ActionResult<List<Product>>> GetProducts()
 		{
-			var products = await _context.Products.AsNoTracking().ToListAsync();
-			return Ok(products);
+            return Ok(await _repository.GetProductsAsync());
 		}
 
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Product>> GetProduct(int id)
 		{
-			return await _context.Products.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+			return await _repository.GetProductByIdAsync(id);
 		}
-	}
+
+		[HttpGet("brands")]
+		public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
+		{
+			return Ok(await _repository.GetProductBrandsAsync());
+		}
+
+        [HttpGet("types")]
+        public async Task<ActionResult<List<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repository.GetProductTypesAsync());
+        }
+    }
 }
 
