22500048b2eee1273b1d3e357dd76a7ba2660be4
taraswithjazz <43855967+taraswithjazz@users.noreply.github.com>
2023-03-31 16:27:29 +0300

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Infrastracture.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductsController : ControllerBase
+	{
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+		public async Task<ActionResult<List<Product>>> GetProducts()
+		{
+			var products = await _context.Products.AsNoTracking().ToListAsync();
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id)
+		{
+			return await _context.Products.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+		}
+	}
+}
+
---
+++ Infrastracture/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastracture.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
